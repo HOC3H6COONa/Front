43c647fc36a94ab215dcc052dc75f53dce47452c
HOC3H6COONa <moresee-2012@mail.ru>
2024-01-07 18:48:07 +0300

    android build

--- front/src/Redux/map-reducer.js
+++
@@ -1,8 +0,0 @@
-let initialState ={
-    City: 'Cairo'
-}
-
-export const mapReducer = (state = initialState,action)=>{
-        return state;
-};
-
--- front/src/api/AxiosInstances/DefaultInstance.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
- export const DefaultInstance = axios.create({
-    withCredentials: true,
-    baseURL: `http://127.0.0.1:8000/`,
-});
